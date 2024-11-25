883b25ddc297b625ba7d0e74e20ed82f223ccd6e
hecsaerod <hecsaerod@alu.edu.gva.es>
2024-11-25 11:26:22 +0100

    Practica 2.3 copiada, compilada y ejecutada

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -16,5 +16,60 @@ namespace ejercicioTelegrama
         {
             InitializeComponent();
         }
+
+        private void boton_Calcular_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = 'o';
+            int numPalabras = 0;
+            double coste;
+            //Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente?
+            if (chkUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+            //Obtengo el número de palabras que forma el telegrama
+            string[] palabras = textoTelegrama.Split(' ');
+            numPalabras = palabras.Length;
+
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    numPalabras = numPalabras - 10;
+                    coste = 2.5 + (0.5 * numPalabras);
+                }
+            }
+            else //Si el telegrama es urgente
+            {
+                if (tipoTelegrama == 'u')
+                {
+
+                    if (numPalabras <= 10)
+
+                    {
+                        coste = 5;
+                    }
+                    else
+                    {
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                    }
+                }
+                else
+                {
+
+                    coste = 0;
+
+                }
+            }
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
     }
 }
